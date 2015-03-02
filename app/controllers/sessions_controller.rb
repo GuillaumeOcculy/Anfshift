0cd7359683a9d510dc85e3875123cc4a673785e5
GuillaumeOcculy <occuly.guillaume@gmail.com>
2015-03-02 17:23:22 +0100

    Authenticate user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
-      redirect_to @user
+    if user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+      sign_in user
+      redirect_to user
     else
       render :new
     end
   end
 
   def destroy
+    reset_session
+    redirect_to root_path
   end
 end
