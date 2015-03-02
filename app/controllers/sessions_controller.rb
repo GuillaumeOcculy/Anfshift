e973fe79cb8943dc78562c033cfc8a0b5a24b4e9
GuillaumeOcculy <occuly.guillaume@gmail.com>
2015-03-02 17:01:00 +0100

    Add sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
-    
+    @user = User.new
   end
 
   def create
+    if @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def destroy
